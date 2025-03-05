b2bdf0166fc35dcd95f0095aa11031295e80f20f
NamDoHyeon2 <skaehgus119@gmail.com>
2025-03-05 20:38:46 +0900

    [Remove] ProjectPositionTag, ProjectSkillTag 중간테이블 삭제

--- src/main/java/hs/kr/backend/devpals/domain/project/repository/ProjectPositionTagRepository.java
+++
@@ -1,7 +0,0 @@
-package hs.kr.backend.devpals.domain.project.repository;
-
-import hs.kr.backend.devpals.domain.project.entity.ProjectPositionTagEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectPositionTagRepository extends JpaRepository<ProjectPositionTagEntity, Long> {
-}
--- src/main/java/hs/kr/backend/devpals/domain/project/repository/ProjectSkillTagRepository.java
+++
@@ -1,7 +0,0 @@
-package hs.kr.backend.devpals.domain.project.repository;
-
-import hs.kr.backend.devpals.domain.project.entity.ProjectSkillTagEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectSkillTagRepository extends JpaRepository<ProjectSkillTagEntity, Long> {
-}
