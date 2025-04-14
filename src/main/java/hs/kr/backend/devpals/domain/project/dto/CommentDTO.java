d53e88fdd0ad97aa5af876e52d17f926767839c5
NamDoHyeon2 <skaehgus119@gmail.com>
2025-04-14 19:23:50 +0900

    [Feat] 스웨거 상세설명 추가

--- src/main/java/hs/kr/backend/devpals/domain/project/dto/CommentDTO.java
+++ src/main/java/hs/kr/backend/devpals/domain/project/dto/CommentDTO.java
@@ -1,22 +1,41 @@
 package hs.kr.backend.devpals.domain.project.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import hs.kr.backend.devpals.domain.project.entity.CommentEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
+@Schema(description = "댓글 DTO")
 public class CommentDTO {
-    private Long id;
-    private Long project_id;
-    private Long user_id;
-    private String content;
-    private LocalDateTime created_at;
-    private LocalDateTime updated_at;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Schema(description = "댓글 ID", example = "12", accessMode = Schema.AccessMode.READ_ONLY)
+    private Long id;
+
+    @Schema(description = "댓글 내용", example = "이 프로젝트 정말 기대돼요!")
+    private String content;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Schema(description = "댓글 작성자 정보", accessMode = Schema.AccessMode.READ_ONLY)
+    private ProjectUserResponse user;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Schema(description = "대댓글 수", example = "0", accessMode = Schema.AccessMode.READ_ONLY)
+    private int commentCount;
+
+    public static CommentDTO fromEntity(CommentEntity comment) {
+        return CommentDTO.builder()
+                .id(comment.getId())
+                .content(comment.getContent())
+                .user(ProjectUserResponse.fromEntity(comment.getUser()))
+                .commentCount(0)
+                .build();
+    }
 }
