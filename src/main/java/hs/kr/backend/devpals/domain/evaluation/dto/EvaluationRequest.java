8623ea73376e05506ba83a99bf73b585f89f6b0e
leejinuk <leejinouk123@kakao.com>
2025-05-07 18:55:29 +0900

    [Refactor]: 평가 제출 DTO 구조 변경(일괄저장->분할저장)

--- src/main/java/hs/kr/backend/devpals/domain/evaluation/dto/EvaluationRequest.java
+++ src/main/java/hs/kr/backend/devpals/domain/evaluation/dto/EvaluationRequest.java
@@ -1,9 +1,11 @@
 package hs.kr.backend.devpals.domain.evaluation.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
-
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
@@ -15,20 +17,9 @@ public class EvaluationRequest {
     @Schema(description = "프로젝트 ID", example = "1234")
     private Long projectId;
 
-    @Schema(description = "평가 항목 목록")
-    private List<EvaluationItem> evaluations;
+    @Schema(description = "평가 대상 유저 ID", example = "17")
+    private Long evaluateeId;
 
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    @Schema(description = "단일 평가 항목 DTO")
-    public static class EvaluationItem {
-
-        @Schema(description = "평가 대상 유저 ID", example = "17")
-        private Long evaluateeId;
-
-        @Schema(description = "평가 점수 배열", example = "[5, 2, 3, 2, 1, 4]")
-        private List<Integer> scores;
-    }
+    @Schema(description = "점수 배열", example = "[5, 4, 3, 3, 2, 5]")
+    private List<Integer> scores;
 }
