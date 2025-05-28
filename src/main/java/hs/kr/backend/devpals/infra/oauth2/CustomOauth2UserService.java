a7d111c5e8fa2af898b61a49fa20cc0e2206e988
NamDoHyeon2 <skaehgus119@gmail.com>
2025-05-28 21:18:22 +0900

    [Remove] 주석 제거

--- src/main/java/hs/kr/backend/devpals/infra/oauth2/CustomOauth2UserService.java
+++ src/main/java/hs/kr/backend/devpals/infra/oauth2/CustomOauth2UserService.java
@@ -133,13 +133,10 @@ public class CustomOauth2UserService extends DefaultOAuth2UserService {
                 );
 
         List<Map<String, Object>> emailList = response.getBody();
-        log.info("🔍 [GitHub Email API 응답] 시작 ---------");
         if (emailList != null) {
             for (Map<String, Object> emailInfo : emailList) {
-                log.info("📧 emailInfo: {}", emailInfo);
             }
         } else {
-            log.warn("⚠️ GitHub email 응답이 null 입니다.");
         }
 
         String primaryEmail = emailList.stream()
@@ -148,8 +145,6 @@ public class CustomOauth2UserService extends DefaultOAuth2UserService {
                 .findFirst()
                 .orElse(null);
 
-        log.info("✅ 선택된 Primary Email: {}", primaryEmail);
-        log.info("🔍 [GitHub Email API 응답] 끝 ---------");
         return primaryEmail;
     }
 }
