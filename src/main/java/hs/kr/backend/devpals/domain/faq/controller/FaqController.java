2b708830c1a8175788bab12fa8fdc8ee880671b7
NamDoHyeon2 <skaehgus119@gmail.com>
2025-06-02 18:38:11 +0900

    [Fix] Faq User, Admin 기능 분리

--- src/main/java/hs/kr/backend/devpals/domain/faq/controller/FaqController.java
+++ src/main/java/hs/kr/backend/devpals/domain/faq/controller/FaqController.java
@@ -18,21 +18,6 @@ import java.util.List;
 public class FaqController {
     private final FaqService faqservice;
 
-    @PostMapping
-    @Operation(
-            summary = "FAQ 작성",
-            description = "관리자가 새로운 FAQ를 작성합니다. 질문과 답변을 입력받습니다.",
-            responses = {
-                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "FAQ 작성 성공"),
-                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청 - 유효하지 않은 입력 등 발생")
-            }
-    )
-    public ResponseEntity<ApiResponse<String>> createFaq(
-            @RequestHeader("Authorization") String token,
-            @RequestBody FaqDTO faqDTO) {
-        return faqservice.createFaq(token, faqDTO);
-    }
-
     @GetMapping
     @Operation(
             summary = "전체 FAQ 조회",
@@ -46,21 +31,6 @@ public class FaqController {
         return faqservice.getAllFaq(keyword);
     }
 
-    @PutMapping("/{faqId}")
-    @Operation(
-            summary = "FAQ 수정",
-            description = "기존 FAQ의 질문 및 답변 내용을 수정합니다. FAQ ID와 함께 수정할 내용을 전달해야 합니다.",
-            responses = {
-                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "FAQ 수정 성공"),
-                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청 - 존재하지 않는 FAQ ID 등")
-            }
-    )
-    public ResponseEntity<ApiResponse<String>> updateFaq(
-            @RequestHeader("Authorization") String token,
-            @PathVariable Long faqId,
-            @RequestBody FaqDTO faqDTO) {
-        return faqservice.updateFaq(token, faqId, faqDTO);
-    }
 
     @GetMapping("/{faqId}")
     @Operation(
@@ -72,23 +42,8 @@ public class FaqController {
             }
     )
     public ResponseEntity<ApiResponse<FaqDTO>> getFaq(
-            @RequestHeader("Authorization") String token,
             @PathVariable Long faqId) {
-        return faqservice.getFaq(token, faqId);
+        return faqservice.getFaq(faqId);
     }
 
-    @DeleteMapping("/{faqId}")
-    @Operation(
-            summary = "FAQ 삭제",
-            description = "FAQ ID를 기반으로 특정 FAQ를 삭제합니다.",
-            responses = {
-                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "FAQ 삭제 성공"),
-                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청 - 존재하지 않는 ID 등")
-            }
-    )
-    public ResponseEntity<ApiResponse<String>> deleteFaq(
-            @RequestHeader("Authorization") String token,
-            @PathVariable Long faqId){
-        return faqservice.deleteFaq(token, faqId);
-    }
 }
---
+++ src/main/java/hs/kr/backend/devpals/domain/faq/service/FaqAdminService.java
@@ -0,0 +1,74 @@
+package hs.kr.backend.devpals.domain.faq.service;
+
+import hs.kr.backend.devpals.domain.faq.dto.FaqDTO;
+import hs.kr.backend.devpals.domain.faq.entity.FaqEntity;
+import hs.kr.backend.devpals.domain.faq.repository.FaqRepository;
+import hs.kr.backend.devpals.domain.user.entity.UserEntity;
+import hs.kr.backend.devpals.domain.user.repository.UserRepository;
+import hs.kr.backend.devpals.global.common.ApiResponse;
+import hs.kr.backend.devpals.global.exception.CustomException;
+import hs.kr.backend.devpals.global.exception.ErrorException;
+import hs.kr.backend.devpals.global.jwt.JwtTokenValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class FaqAdminService {
+
+    private final FaqRepository faqRepository;
+    private final JwtTokenValidator jwtTokenValidator;
+    private final UserRepository userRepository;
+
+    public ResponseEntity<ApiResponse<String>> createFaq(String token, FaqDTO request) {
+        validateAdmin(token);
+
+        FaqEntity faq = FaqEntity.from(request);
+
+        faqRepository.save(faq);
+
+        return ResponseEntity.ok(new ApiResponse<>(200, true, "FAQ 작성 성공", null));
+    }
+
+    @Transactional
+    public ResponseEntity<ApiResponse<String>> updateFaq(String token, Long id, FaqDTO faqDTO) {
+        validateAdmin(token);
+
+        FaqEntity faq = faqRepository.findById(id).orElse(null);
+        if (faq == null) {
+            return ResponseEntity.ok(new ApiResponse<>(404, false, "FAQ글이 존재하지 않습니다", null));
+        }
+
+        faq.update(faqDTO.getTitle(), faqDTO.getContent());
+        faqRepository.save(faq);
+
+        return ResponseEntity.ok(new ApiResponse<>(200, true, "FAQ 수정 완료", null));
+    }
+    @Transactional
+    public ResponseEntity<ApiResponse<String>> deleteFaq(String token, Long faqId) {
+        validateAdmin(token);
+
+        FaqEntity faq = faqRepository.findById(faqId).orElse(null);
+        if (faq == null) {
+            return ResponseEntity.ok(new ApiResponse<>(404, false, "FAQ글이 존재하지 않습니다", null));
+        }
+
+        faqRepository.delete(faq);
+        return ResponseEntity.ok(new ApiResponse<>(200, true, "FAQ 삭제 완료", null));
+    }
+
+    public void validateAdmin(String token) {
+        Long userId = jwtTokenValidator.getUserId(token);
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorException.USER_NOT_FOUND));
+
+        if (!user.getIsAdmin()) {
+            throw new CustomException(ErrorException.NO_PERMISSION);
+        }
+    }
+}
