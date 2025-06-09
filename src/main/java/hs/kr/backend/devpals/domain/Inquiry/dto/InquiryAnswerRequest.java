c6c369894dfb760db74c854f5337cb31cd10ef53
NamDoHyeon2 <skaehgus119@gmail.com>
2025-06-09 14:50:52 +0900

    [Fix] 답변 작성, 수정 RequestParam에서 Body로 수정

---
+++ src/main/java/hs/kr/backend/devpals/domain/Inquiry/dto/InquiryAnswerRequest.java
@@ -0,0 +1,12 @@
+package hs.kr.backend.devpals.domain.Inquiry.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InquiryAnswerRequest {
+    private String answer;
+}
