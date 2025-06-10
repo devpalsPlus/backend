2d4b238aaf40cf8e9c664422ee3ad6fa49207fe1
NamDoHyeon2 <skaehgus119@gmail.com>
2025-06-10 19:25:21 +0900

    [Feat] 문의 답변시 알람 보낼 수 있는 기능 설정

--- src/main/java/hs/kr/backend/devpals/domain/user/entity/alarm/InquiryAlarmEntity.java
+++ src/main/java/hs/kr/backend/devpals/domain/user/entity/alarm/InquiryAlarmEntity.java
@@ -1,5 +1,35 @@
 package hs.kr.backend.devpals.domain.user.entity.alarm;
 
-//TODO: 추후구현
-public class InquiryAlarmEntity {
+import hs.kr.backend.devpals.domain.Inquiry.entity.InquiryEntity;
+import hs.kr.backend.devpals.domain.user.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import static hs.kr.backend.devpals.global.constants.AlarmFilterConstants.INQUIRY;
+import static hs.kr.backend.devpals.global.constants.AlarmFilterConstants.INQUIRY_INT_VALUE;
+
+@Entity
+@DiscriminatorValue(INQUIRY) // "INQUIRY"
+@NoArgsConstructor
+@Getter
+@Table(name = "InquiryAlarm") // 테이블 이름 지정
+public class InquiryAlarmEntity extends AlarmEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "inquiry_id", nullable = false)
+    private InquiryEntity inquiry;
+
+    public InquiryAlarmEntity(UserEntity receiver, String content, InquiryEntity inquiry) {
+        super(receiver, content, inquiry.getId());
+        this.inquiry = inquiry;
+    }
+
+    @Override
+    public Integer getAlarmFilterIntValue() {
+        return INQUIRY_INT_VALUE;
+    }
 }
