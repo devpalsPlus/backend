a244b24bd0afc05b39299dcf1d6eb198522c756e
NamDoHyeon2 <skaehgus119@gmail.com>
2025-06-11 21:33:22 +0900

    [Fix] Swagger 설정 수정

--- src/main/java/hs/kr/backend/devpals/domain/Inquiry/repository/InquiryRepository.java
+++ src/main/java/hs/kr/backend/devpals/domain/Inquiry/repository/InquiryRepository.java
@@ -13,7 +13,7 @@ public interface InquiryRepository extends JpaRepository<InquiryEntity, Long> {
     List<InquiryEntity> findByUserIdOrderByCreatedAtDesc(Long userId);
     List<InquiryEntity> findAllByOrderByCreatedAtDesc();
     List<InquiryEntity> findByUserEmailOrderByCreatedAtDesc(String email);
-    
+
     @Query("SELECT i FROM InquiryEntity i WHERE i.user.email = :email AND i.createdAt BETWEEN :startDate AND :endDate ORDER BY i.createdAt DESC")
     List<InquiryEntity> findInquiriesByEmailAndDate(
             @Param("email") String email,
